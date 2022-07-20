8771187e4ce01fa91bf4bdacf5ec23257d967d67
MMoeJs <mernmoe@gmail.com>
2022-07-20 11:47:24 +0100

    Deployed Website

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,7 @@ import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './Components/Dashboard/Dashboard';
 import NavBar from './Components/NavBar/NavBar';
 function App() {
-  return (
-    <HashRouter>
-      Mohamed
-      <Routes>
-        Mohamed
-        <Route path='/' element={<NavBar></NavBar>}>
-          <Route
-            path='/Admin'
-            element={
-              <div className='flex w-screen h-screen items-center justify-center'>
-                Chkeck the Dashboard ,This is just a trial ,Start your order to
-                get the full website
-              </div>
-            }
-          ></Route>
-          <Route path='/Dashboard' element={<Dashboard></Dashboard>}></Route>
-        </Route>
-      </Routes>
-    </HashRouter>
-  );
+  return <div>Mohamed</div>;
 }
 
 export default App;
